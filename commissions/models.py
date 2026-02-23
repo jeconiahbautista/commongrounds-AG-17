c6714d4e1fec7becc2ead4beaff900a3ef7bad05
mikacguro <mikaellaclg@gmail.com>
2026-02-23 18:42:10 +0800

    Modified models.py and views.py, and added necessary templates for request list view

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'commission type'
+        verbose_name_plural = 'commission types'
+
+class Commission(models.Model):
+    commission_type = models.ForeignKey(
+        CommissionType,
+        on_delete=models.CASCADE,
+        related_name="commissions"
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True, null=False)
+    updated_on = models.DateTimeField(auto_now=True, null=False)
+
+    def __str__(self):
+        return '{} from {}'.format(self.title, self.commission_type)
+    
+    def get_absolute_url(self):
+        return reverse('requests')
+    
+    class Meta:
+        ordering = ['created_on']
+        verbose_name = 'commission'
+        verbose_name_plural = 'commissions'
+
+
+
--- commissions/urls.py
+++ commissions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path ('', index, name='index'),
+    path('', index, name='index'),
+    path('requests', RequestListView.as_view(), name="requests" )
 ]
 
 app_name = "commissions"
