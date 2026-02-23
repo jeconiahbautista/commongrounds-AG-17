5a1124144a31d126a887769249eb8da8fadd6df4
mikacguro <mikaellaclg@gmail.com>
2026-02-23 18:42:10 +0800

    Modified models.py and views.py, and added necessary templates for request list view

--- commissions/views.py
+++ commissions/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from .models import CommissionType, Commission
 
 def index(request):
     return HttpResponse('Hello! This is the Commissions Requests Apps.')
 
-# Create your views here.
+def request_list(request):
+
+    commissions = Commission.objects.all()
+    ctx = {
+        'commissions' : commissions
+    }
+
+    return render(request, "commissions/request_list.html", ctx)
+
+class RequestListView(ListView):
+    model = Commission
+    template_name = 'commissions/request_list.html'
+
+
+
