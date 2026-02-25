728ec49e3328b90b32deed9b7bc5e439bc07ba2e
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-02-26 01:06:53 +0800

    Fixed the name of the project category instances on admin view.

--- diyprojects/models.py
+++ diyprojects/models.py
@@ -5,6 +5,9 @@ class ProjectCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=False, blank=False)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ['name',]
         verbose_name = 'Project Category'
