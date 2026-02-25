1ce3e7cd5720d313a79b167d6503cb1a2ee9aa02
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-02-25 23:58:20 +0800

    Added appropriate admin panels for the list and detail view of the project.

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, ProjectCategory
 
-# Register your models here.
+class ProjectCategoryInline(admin.TabularInline):
+    model = ProjectCategory
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    inlines = []
+
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    model = ProjectCategory
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
