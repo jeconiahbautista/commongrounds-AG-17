7c77919ddffb69824727b8647d9acb5d6fba7328
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-02-25 23:16:14 +0800

    Created a diy projects detail view.

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def diyprojects_list(request):
     return render(request, 'diy-projects_list.html')
+
+def diyprojects_detail(request):
+    return render(request, 'diy-projects_detail.html')
