8c6db464bcc428b957ff34d41d4ac287fe36b0af
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-02-25 23:49:07 +0800

    Created models for the app and made migrations.

--- diyprojects/models.py
+++ diyprojects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=False, blank=False)
+
+    class Meta:
+        ordering = ['name',]
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ProjectCategory,
+        on_delete=models.SET_NULL,
+        related_name='projects',
+        null=True
+    )
+    description = models.TextField(null=False, blank=False)
+    materials = models.TextField(null=False, blank=False)
+    steps = models.TextField(null=False, blank=False)
+    created_on = models.DateTimeField(null=False)
+    updated_on = models.DateTimeField(null=False)
+
+    class Meta:
+        ordering = ['created_on',]
+
--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('projects', diyprojects_list, name='diyprojects_list'),
     path('project', diyprojects_list, name='diyprojects_list'),
 ]
+
+app_name = 'diyprojects'
