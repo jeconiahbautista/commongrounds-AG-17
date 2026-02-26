f986b7a467f044fdf4e166ea1e09724d15d882f8
jem calipjo <jeremy.louise.calipjo@student.ateneo.edu>
2026-02-26 16:00:52 +0800

    Updated the views to take in the models.

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Product
+
+def index(request):
+    return HttpResponse('Welcome to the Merchandise Store.')
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'product_list.html'
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product_detail.html'
