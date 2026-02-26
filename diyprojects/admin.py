1702ba8e364108dfbe2dd5067c4a6b44e731c897
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-02-26 18:02:41 +0800

    Updated the project category admin panel inline.

--- diyprojects/admin.py
+++ diyprojects/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Project, ProjectCategory
 
-class ProjectInline(admin.TabularInline):
+class ProjectInline(admin.StackedInline):
     model = Project
 
 class ProjectCategoryAdmin(admin.ModelAdmin):
