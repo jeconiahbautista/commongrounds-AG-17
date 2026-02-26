777370dbe86373a292f0d4791ef29f544e37cb3e
jem calipjo <jeremy.louise.calipjo@student.ateneo.edu>
2026-02-26 16:03:13 +0800

    Made migrations for the models.
    Updated admin.py to have ProductType admin and Product as inline.

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, ProductType
 
-# Register your models here.
+
+class ProductInline(admin.TabularInline):
+    model = Product
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+    inlines = [ProductInline, ]
+
+admin.site.register(ProductType, ProductTypeAdmin)
