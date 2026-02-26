41e5a4598f050d382b3dda95524ac3754d36330d
jem calipjo <jeremy.louise.calipjo@student.ateneo.edu>
2026-02-26 17:26:39 +0800

    Added Product admin.
    Removed index path in the views and urls.
    
    Fixed naming in templates.

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
 
-from .views import index, ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView
 
 
 urlpatterns = [
-    path('', index, name = 'index'),
     path('items/', ProductListView.as_view(), name='product_list'),
     path('item/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
