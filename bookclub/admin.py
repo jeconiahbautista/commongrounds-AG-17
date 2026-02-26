2be68cfbedbce4af5107bec6d55172ffd156468e
Charlisze Manansala <charlisze.manansala@student.ateneo.edu>
2026-02-26 17:29:15 +0800

    Added the Admin.py

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import Genre, Book
 
-class BookInLine(admin.TabularInline):
+class BookInLine(admin.StackedInline):
     model = Book
 
 class GenreAdmin(admin.ModelAdmin):
-    inlines = Genre
-    inlines = []
+    model = Genre
+    inlines = [BookInLine,]
 
 class BookAdmin(admin.ModelAdmin):
     model = Book
+    search_fields = ('title', 'author', 'publication_year')
+    list_display = ('title', 'genre', 'author', 'publication_year', 'created_on', 'updated_on')
+    list_filter = ('genre',)
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('title', 'genre', 'author', 'publication_year',)
+            ]
+        })
+    ]
 
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Book, BookAdmin)
-
-# Register your models here.
