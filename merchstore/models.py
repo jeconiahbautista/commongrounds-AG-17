b6181a35e4fadf15ac7b7bd7ec7c2334e93326f5
jem calipjo <jeremy.louise.calipjo@student.ateneo.edu>
2026-02-26 15:50:29 +0800

    Modified models.py to have Product and ProductType models.

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+       return reverse('merchstore:product_list', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['name']
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        ProductType,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='products'
+    )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=20,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('merchstore:product_detail', args=[str(self.id)])
+    
+    class Meta:
+        ordering = ['name']
