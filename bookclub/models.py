2e8930942a52bc35b9b2ec936f03467ff5aaf72b
Charlisze Manansala <charlisze.manansala@student.ateneo.edu>
2026-03-03 14:10:38 +0800

    Formatted the codes according to PEP8 style.

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,36 +1,39 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
     def __str__(self):
         return self.name
-    
-    class Meta: 
-        ordering = ['name',]
+
+    class Meta:
+        ordering = [
+            "name",
+        ]
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     genre = models.ForeignKey(
-        Genre,
-        on_delete=models.SET_NULL,
-        related_name = "genres",
-        null = True
+        Genre, on_delete=models.SET_NULL, related_name="genres", null=True
     )
     author = models.CharField()
     publication_year = models.IntegerField()
-    created_on = models.DateTimeField(auto_now_add=True, null = False)
+    created_on = models.DateTimeField(auto_now_add=True, null=False)
     updated_on = models.DateTimeField(auto_now=True, null=False)
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
-        return reverse('bookclub:book-detail', args=[str(self.pk)])
-    
+        return reverse("bookclub:book-detail", args=[str(self.pk)])
+
     class Meta:
-        ordering = ['created_on',]
-        verbose_name = 'book'
+        ordering = [
+            "created_on",
+        ]
+        verbose_name = "book"
         verbose_name_plural = "books"
