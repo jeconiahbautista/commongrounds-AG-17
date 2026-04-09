a11d9e2fe9decea255f911bc6e1cdb4648b4f59c
jem calipjo <jeremy.louise.calipjo@student.ateneo.edu>
2026-04-09 21:30:12 +0800

    Modified Product model and added Transaction model

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
 
+from accounts.models import Profile
+from django.core.validators import MinValueValidator
+
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -16,16 +19,94 @@ class ProductType(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(
-        ProductType, null=True, on_delete=models.SET_NULL, related_name="products"
+        ProductType, 
+        null=True, 
+        on_delete=models.SET_NULL, 
+        related_name="products",
+        editable=False
+    )
+    owner = models.ForeignKey(
+        Profile, 
+        on_delete=models.CASCADE, 
+        related_name="products"
+    )
+    product_image = models.ImageField(
+        upload_to='product_images/', 
+        null=True, 
+        blank=True
     )
     description = models.TextField()
-    price = models.DecimalField(max_digits=20, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2
+    )
+    stock = models.PositiveIntegerField()
+    STATUS_CHOICES = [
+        ('AVAILABLE', 'Available'),
+        ('ON_SALE', 'On sale'),
+        ('OUT_OF_STOCK', 'Out of stock'),
+    ]
+    status = models.CharField(
+        max_length=15,
+        choices=STATUS_CHOICES,
+        default='AVAILABLE',
+    )
+
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.status = 'OUT_OF_STOCK'
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("merchstore:product_detail", args=[str(self.id)])
+        return reverse(
+            "merchstore:product_detail", 
+            args=[str(self.id)]
+        )
 
     class Meta:
         ordering = ["name"]
+
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        Profile,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='transactions'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='transactions'
+    )
+    amount = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    STATUS_CHOICES = [
+        ('ON_CART', 'On cart'),
+        ('TO_PAY', 'To Pay'),
+        ('TO_SHIP', 'To Ship'),
+        ('TO_RECEIVE', 'To Receive'),
+        ('DELIVERED', 'Delivered'),
+    ]
+    status = models.CharField(
+        max_length=15,
+        choices=STATUS_CHOICES,
+        default='ON_CART',
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {} at {}".format(
+            self.buyer,
+            self.product,
+            self.amount
+        )
+
+    class Meta:
+        ordering = ['created_on']
+
+    
