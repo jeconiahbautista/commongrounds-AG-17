e4d831e1ccdced76015d1aaff063c4433c567208
Joshua <joshua.patrick.bandola@student.ateneo.edu>
2026-05-02 20:41:59 +0800

    Added an account registration. Styled the dashboard with login and registration templates.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,60 @@
 from django import forms
 from .models import Profile
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ["display_name"]
+
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "login-field",
+                "placeholder": "Enter a valid username",
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "login-field",
+                "placeholder": "Enter a password with at least 8 characters.",
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "login-field",
+                "placeholder": "Re-enter your password",
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
+
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "login-field",
+                "placeholder": "Enter your username",
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "login-field",
+                "placeholder": "Enter your password",
+            }
+        )
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from accounts.decorators import role_required
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Profile
-from .forms import ProfileUpdateForm
+from .forms import ProfileUpdateForm, CustomLoginForm, CustomUserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, "register.html", {"form": form})
 
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
     form_class = ProfileUpdateForm
-    template_name = "accounts/profile_form.html"
+    template_name = "profile_form.html"
 
     def get_object(self, queryset=None):
         return self.request.user.profile
@@ -22,9 +36,15 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
         )
 
 
+class CustomLoginView(LoginView):
+    form_class = CustomLoginForm
+    template_name = "registration/login.html"
+
+
 @login_required
 def dashboard(request):
-    return render(request, "accounts/dashboard.html")
+    return render(request, "dashboard.html")
+
 
 def permission_denied_view(request):
-    return render(request, "accounts/permission_denied.html")
+    return render(request, "permission_denied.html")
