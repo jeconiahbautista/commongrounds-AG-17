86c44440ba9af74764ee977c8fd7f130fdf43c16
mikaellacaluag <mikaella.caluag@student.ateneo.edu>
2026-05-07 17:23:57 +0800

    modify UI

--- localevents/forms.py
+++ localevents/forms.py
@@ -18,6 +18,30 @@ class EventForm(forms.ModelForm):
             "status",
         ]
 
+        widgets = {
+            
+            "title": forms.TextInput(attrs={
+                "placeholder": "Enter event title..."
+            }),
+            "description": forms.Textarea(attrs={
+                "rows": 4,
+                "placeholder": "Describe the event..."
+            }),
+            "start_time": forms.TextInput(attrs={
+                "placeholder": "Add start time..."
+            }),
+            "end_time": forms.TextInput(attrs={
+                "placeholder": "Add end time..."
+            }),
+            "location": forms.TextInput(attrs={
+                "placeholder": "Enter location..."
+            }),
+            "event_capacity": forms.TextInput(attrs={
+                "placeholder": "Add event time..."
+            }),
+
+        }
+
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
